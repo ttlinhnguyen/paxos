3dbd014c2fd969bfcdb7d246e0e4f4946850cae8
Linh Nguyen <linhtunguyen.1311@gmail.com>
2023-11-02 15:36:49 +1030

    default controller

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class Controller {
-    HashMap<Integer, Acceptor> acceptors = new HashMap<>(); // ID: members.Acceptor
-    HashMap<Integer, Proposer> proposers = new HashMap<>(); // ID: members.Proposer
+    HashMap<Integer, Acceptor> acceptors; // ID: members.Acceptor
+    HashMap<Integer, Proposer> proposers; // ID: members.Proposer
     Controller(ControllerBuilder builder) {
         acceptors = builder.acceptors;
         proposers = builder.proposers;
@@ -25,26 +25,35 @@ public class Controller {
         public DefaultController() {}
         public Controller build() {
             Integer[] delays = {0, 200, 400, 50, 100, 150, 200, 250, 300};
-            Controller.ControllerBuilder builder = new Controller.ControllerBuilder();
-            for (int i=1; i<4; i++) builder.addProposer(new Proposer(i, 1000 + i, delays[i-1], 8));
-            for (int i=4; i<10; i++) builder.addAcceptor(new Acceptor(i, 1000+i, delays[i-1]));
+            ControllerBuilder builder = new ControllerBuilder(9, 1000);
+            for (int i=1; i<10; i++) {
+                if (i<4) builder = builder.addProposer(i, delays[i-1]);
+                else builder = builder.addAcceptor(i, delays[i-1]);
+            }
 
             return builder.build();
         }
     }
 
     public static class ControllerBuilder {
+        int startingPort;
+        int numMembers;
         HashMap<Integer, Acceptor> acceptors = new HashMap<>(); // ID: members.Acceptor
         HashMap<Integer, Proposer> proposers = new HashMap<>(); // ID: members.Proposer
-        public ControllerBuilder() {}
+        public ControllerBuilder(int numMembers, int startingPort) {
+            this.numMembers = numMembers;
+            this.startingPort = startingPort;
+        }
 
-        public ControllerBuilder addProposer(Proposer proposer) {
+        public ControllerBuilder addProposer(int uuid, int delay) {
+            Proposer proposer = new Proposer(uuid, startingPort++, delay, numMembers-1);
             proposers.put(proposer.getUUID(), proposer);
             acceptors.put(proposer.getUUID(), proposer);
             return this;
         }
 
-        public ControllerBuilder addAcceptor(Acceptor acceptor) {
+        public ControllerBuilder addAcceptor(int uuid, int delay) {
+            Acceptor acceptor = new Acceptor(uuid, startingPort++, delay);
             acceptors.put(acceptor.getUUID(), acceptor);
             return this;
         }
